7013dfc1c86e4b6122b8fd9d8388e41b6590f53f
Carolina Ruiz <icruiz92@outlook.com>
2022-08-10 09:44:35 -0300

    Segunda versión TP Final de Entorno de Programación

--- menu.sh
+++ menu.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 menu () {
-	echo "¿Qué script desea usar?"
+	echo " "
+	echo "¿Qué script desea usar? Elija el número de la opción."
 	echo " "
 
 	# se selecciona el script para ejecutar:
 
 	select opcion in $( ls -1 "./Codigos/" | grep \.sh$ ) "INTRO" "EXIT"
 	do
+		echo " "
 		if [ -e $opcion ]; then
-			echo "El script no existe. VUelva a intentar."
+			echo "El script no existe. Vuelva a intentar."
 			echo " "
 			read -p "Presione cualquier tecla para continuar."
 		elif [ $opcion == "EXIT" ]; then
@@ -19,7 +21,7 @@ menu () {
 			echo
 			read -p "Presione cualquier tecla para continuar."
 		else
-			archivo $opcion
+			texto $opcion
 			break
 		fi
 		menu
@@ -28,17 +30,20 @@ menu () {
 }
 
 texto () {
-	echo "¿Qué texto quiere analizar con el script?"
+	echo " "
+	echo "¿Qué texto quiere analizar con el script? Escriba el número de la opción querida."
 	echo " "
 
 	# Se selecciona el texto para analizar:
 
 	select archivo in $( ls -1 ./Texto/ | grep \.txt$ ) "Volver"; do
+		echo " "
 		if [ $archivo == "Volver" ]; then
 			menu
 			break
 		elif [ -e $archivo ]; then
 			echo "El archivo no existe."
+			echo ""
 			read -p "Presione cualquier tecla para continuar."
 		else
 			bash ./Codigos/$1 ./Texto/$archivo
